26c4a365a534d48ecf9b6c9623debaf2150f7ebd
Sam Handler <sam.handler@shopify.com>
2023-04-14 08:30:58 -0600

    use hexadecimal directly

--- lib/opentelemetry/trace/id_generator.lua
+++ lib/opentelemetry/trace/id_generator.lua
@@ -4,22 +4,21 @@ local bit = require 'bit'
 local tohex = bit.tohex
 local fmt = string.format
 local random = util.random
-local FFFFFFFF = 4294967295 -- FFFFFFFF in hexadecimal is 4294967295 in decimal
 
 local _M = {}
 
 function _M.new_span_id()
     return fmt("%s%s",
-        tohex(random(0, FFFFFFFF), 8),
-        tohex(random(0, FFFFFFFF), 8))
+        tohex(random(0, 0xFFFFFFFF), 8),
+        tohex(random(0, 0xFFFFFFFF), 8))
 end
 
 function _M.new_ids()
     return fmt("%s%s%s%s",
-        tohex(random(0, FFFFFFFF), 8),
-        tohex(random(0, FFFFFFFF), 8),
-        tohex(random(0, FFFFFFFF), 8),
-        tohex(random(0, FFFFFFFF), 8)), _M.new_span_id()
+        tohex(random(0, 0xFFFFFFFF), 8),
+        tohex(random(0, 0xFFFFFFFF), 8),
+        tohex(random(0, 0xFFFFFFFF), 8),
+        tohex(random(0, 0xFFFFFFFF), 8)), _M.new_span_id()
 end
 
 return _M
